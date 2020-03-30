2496ffbfefde4c3cef8337702eae5963ad9d4e22
Zakhar Miroshnyk <s14893@pjwstk.edu.pl>
2020-03-30 14:59:55 +0200

    Add project files.

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Library.Models.DTO;
+using Library.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Library.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(IUserRepository userRepository, ILogger<UsersController> logger)
+        {
+            _userRepository = userRepository;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var res = await _userRepository.GetUsers();
+            return Ok(res);
+        }
+
+        [HttpPost(Name = nameof(AddUser))]
+        public async Task<IActionResult> AddUser([FromBody] UserDto user)
+        {
+           var res = await _userRepository.AddUser(user);
+
+            return CreatedAtRoute(nameof(AddUser), res);
+        }
+
+        [HttpGet("{idUser:int}")]
+        public async Task<IActionResult> GetUser([FromRoute] int idUser)
+        {
+            var res = await _userRepository.GetUser(idUser);
+            return Ok(res);
+        }
+        
+    }
+}
---
+++ Api/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using NLog.Web;
+
+namespace Library
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                //RollbarLocator.RollbarInstance.Configure(new RollbarConfig("####"));
+                //RollbarLocator.RollbarInstance.Info("Rollbar is configured properly.");
+
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                //NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                }).ConfigureLogging(logging =>
+                {
+                    //logging.ClearProviders();
+                    //logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                }).UseNLog();
+    }
+}
